aecaa8ed4dd345f7aa9ba3fc847cce1b35984cdf
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2023-10-26 17:15:58 +0530

    Simplified test case

--- k6/amazon.js
+++ k6/amazon.js
@@ -2,47 +2,19 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 10, // Number of virtual users (simulated users)
-  duration: '10s', // Duration of the test in seconds
+  vus: 1, // Virtual Users
+  duration: '10s', // Duration of the test
 };
 
 export default function () {
-  // Define the Amazon homepage URL
-  const baseUrl = 'https://www.amazon.com';
+  // Send an HTTP GET request to the website
+  let response = http.get('https://amazon.com'); // Replace with your website URL
 
-  // Send an HTTP GET request to the Amazon homepage
-  let response = http.get(baseUrl);
-
-  // Check if the homepage request was successful
+  // Check if the response time is greater than 1 second
   check(response, {
-    'Homepage is accessible': (r) => r.status === 200,
+    'Response time is less than 1 second': (r) => r.timings.duration < 1000,
   });
 
-  // Sleep for a short duration (e.g., 2 seconds) to simulate user think time
-  sleep(2);
-
-  // Define the search keyword (e.g., "iPhone")
-  const searchKeyword = 'iPhone';
-
-  // Construct the search URL with the keyword
-  const searchUrl = `${baseUrl}/s?k=${searchKeyword}`;
-
-  // Send an HTTP GET request to perform the search
-  response = http.get(searchUrl);
-
-  // Measure the time until the page loads completely
-  const pageLoadTime = response.timings.duration;
-
-  console.log("==== Response Time")
-  console.log(pageLoadTime)
-
-  // Check if the search results page request was successful
-  check(response, {
-    'Search results page is accessible': (r) => r.status === 200,
-  });
-
-  // Fail the test if pageLoadTime exceeds 5 seconds, otherwise pass it
-  if (pageLoadTime > 5000) {
-    fail(`Page load time exceeded 5 seconds: ${pageLoadTime} ms`);
-  }
+  // Add a sleep period (in this case, 1 second) between requests
+  sleep(1);
 }
