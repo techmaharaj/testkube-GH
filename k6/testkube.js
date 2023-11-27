b876fa22ae2678d92c9ebb1fa5bb5bd477e7e44c
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2023-11-27 10:52:12 +0530

    Update testkube.js

--- k6/testkube.js
+++ k6/testkube.js
@@ -1,18 +1,20 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 export let options = {
-  stages: [
-    { duration: '10s', target: 1 }, // 1 virtual user for 10 seconds
-  ],
+  vus: 1, // Virtual Users
+  duration: '10s', // Duration of the test
 };
 
 export default function () {
   // Send an HTTP GET request to the website
-  let response = http.get('https://testkube.io');
+  let response = http.get('https://amazon.com'); // Replace with your website URL
 
-  // Check the response duration
+  // Check if the response time is greater than 1 second
   check(response, {
-    'Response time is less than 1 second': (r) => r.timings.duration < 1000, // Adjust threshold as needed
+    'Response time is less than 1 second': (r) => r.timings.duration < 1000,
   });
+
+  // Add a sleep period (in this case, 1 second) between requests
+  sleep(1);
 }
